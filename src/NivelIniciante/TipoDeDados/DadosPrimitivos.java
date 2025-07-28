2d9d246d0119c540d978c379a6775536a4f37539
LukReis <lucasesdras8@gmail.com>
2025-07-28 18:36:19 -0300

    Arrumando Erro de importação Modulo iniciante Java10x

---
+++ src/NivelIniciante/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package NivelIniciante.TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 99999999L;
+
+        System.out.println(idade);
+        System.out.println(saldoBancario);
+
+
+    }
+}
